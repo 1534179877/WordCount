70f3b92b1590ddb8ebb2811177c32794dd837424
1534179877 <1534179877@qq.com>
2020-05-20 22:07:45 +0800

    WordCount secend

---
+++ WordCount.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+int *TXTCount(char *filename, char w);
+
+int main(){
+    char filename[30];
+	char w;
+    printf("Input file name: ");
+    scanf("%s", filename);
+	printf("input count way:(input c 字符数 or  w 单词数)\n");
+	scanf("%s",&w);
+
+	TXTCount(filename, w);
+    return 0;
+}
+
+
+
+int *TXTCount(char *filename, char w){
+	FILE  *f;//文件指针
+	char buffer[1003];
+	int bufferL;
+	int i;
+	char c;
+	int blank = 0;
+	int charnum = 0;
+	int wordnum = 0;//每一行的字符数，单词数
+	int CharNum = 0;
+	int WordNum = 0;//总的字符数，单词数
+
+	if((f=fopen(filename,"rb"))==NULL){
+		perror(filename);//读取路径下的文本文件  输出错误
+		return NULL;
+	}
+	while(fgets(buffer,1003,f)!=NULL){
+		bufferL = strlen(buffer);//遍历缓冲区
+	for(i=0; i<bufferL; i++){//遇到空格
+		c=buffer[i];
+		if(c==' '||c=='\t'){
+			!blank&&wordnum++;
+			blank = 1;
+		}
+		else{
+			charnum++;
+			blank=0;
+		}
+	}
+	blank&&wordnum++;//最后一个字符不是空格
+	blank=1;
+		
+	CharNum +=charnum;
+	WordNum+=wordnum;
+
+	charnum = 0;
+    wordnum = 0;
+
+	}
+
+	if(w=='c'){
+		printf("字符数：%d\n",CharNum);
+	}
+	else{
+		printf("单词数：%d\n",WordNum);
+	}
+
+	return 0;
+}
